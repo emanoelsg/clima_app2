99f24e921bb8c99d5b94ce430747269f5fb11104
Airget-01 <dapouco3@gmail.com>
2025-03-26 08:58:41 -0300

    dados carregaod

--- lib/app/view/main_screen.dart
+++ lib/app/view/main_screen.dart
@@ -13,23 +13,29 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Consumer<WeatherController>(
-            builder: (context, value, child) {
-              if (value.isLoading == true) {
-                return Center(child: CircularProgressIndicator());
-              }
-              if (value.error != null) {
-                return Center(child: Text(value.error.toString()));
-              }
-              if (value.weather == null) {
-                return Center(child: Text('Não foiWeatherDetails possível carregar dados'));
-              }return Center(child:Text('dados carregados'));
-            },
-          ),
-        ],
+      appBar: AppBar(title: const Text('')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Consumer<WeatherController>(
+              builder: (context, value, child) {
+                if (value.isLoading == true) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (value.error != null) {
+                  return Center(child: Text(value.error.toString()));
+                }
+                if (value.weather == null) {
+                  return Center(child: Text('Não foi possível carregar dados'));
+                }return Center(child:Text('dados carregados'));
+              },
+            ),],
+          
+        ),
       ),
-    );
+    floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<WeatherController>().fetchWeather(),
+        child: Icon(Icons.refresh),
+      ),);
   }
 }
